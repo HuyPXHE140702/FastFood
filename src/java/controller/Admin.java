d8dfe9e79319ee354b00257fe2ac945a7384855e
HuyPXHE140702 <huypxhe140702@fpt.edu.vn>
2022-09-30 17:31:31 +0700

    remove all unused import

--- src/java/controller/Admin.java
+++ src/java/controller/Admin.java
@@ -6,7 +6,6 @@
 package controller;
 
 import dao.AccountDAO;
-import dao.ShipperDAO;
 import java.io.IOException;
 import java.io.PrintWriter;
 import java.util.List;
--- src/java/dao/FoodDAO.java
+++ src/java/dao/FoodDAO.java
@@ -12,7 +12,6 @@ import java.sql.SQLException;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Vector;
-import model.Category;
 import model.Food;
 
 /**
